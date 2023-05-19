04f811eaf28cb3298c262f28fd4454e994b08a5b
PE5I <116784810+PE5I@users.noreply.github.com>
2023-05-19 00:32:02 -0700

    add service function for creating user

---
+++ src/services/userService.ts
@@ -0,0 +1,20 @@
+import { UniqueConstraintError } from "sequelize";
+import { User } from "../models"
+import { NewUserEntry, UserEntry } from "../utils/types"
+
+const createUser = async (newUser: NewUserEntry) => {
+  try {
+    const createdUser = await User.create(newUser)
+    return createdUser;
+  } catch (err) {
+    if (err instanceof UniqueConstraintError) {
+      throw new Error(`${err.errors[0].message}`)
+    }
+    throw new Error('something unexpected happened')
+  }
+
+}
+
+export default {
+  createUser
+}
