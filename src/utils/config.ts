932b4ea95b8dd49db073f0cd96e95ef3b6e1b2d7
PE5I <116784810+PE5I@users.noreply.github.com>
2023-05-19 00:30:54 -0700

    add test db url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const DATABASE_URL = process.env.DATABASE_URL || "hi"
+export const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_DATABASE_URL
+  : process.env.PROD_DATABASE_URL
 
 // console.log("env", process.env);
