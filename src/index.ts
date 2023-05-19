84c23540d06694fd4b5beec0aad0150aaf1b9905
PE5I <116784810+PE5I@users.noreply.github.com>
2023-05-19 00:32:40 -0700

    convert requests to json and use error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-import express from 'express'
+import express, { ErrorRequestHandler } from 'express'
+import 'express-async-errors' // must be imported before creating any express.Router objects
 import usersRouter from './controllers/user';
 import { connectToDatabase } from './utils/db';
+import { errorHandler } from './utils/middleware';
 
 const app = express()
+app.use(express.json())
+
 
 app.use('/api/users', usersRouter)
+app.use(errorHandler as ErrorRequestHandler)
 
 const start = async () => {
-  await connectToDatabase()
-  app.listen(3001, () => {
-    console.log('Server started');
-  })
+  if (process.env.NODE_ENV !== 'test') {
+    await connectToDatabase()
+    app.listen(3001, () => {
+      console.log('Server started');
+    })
+  }
 }
 
 start()
+
+
+export default app
