cc67ec14392464ff6447fb3f38a6f4ef669ec7a9
PE5I <116784810+PE5I@users.noreply.github.com>
2023-06-20 23:28:54 -0700

    add getAllUsers and getUserById

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,20 +1,59 @@
 import { UniqueConstraintError } from 'sequelize';
-import { User } from '../models';
+import { Post, Topic, User } from '../models';
 import { NewUserEntry, UserEntry } from '../utils/types';
+import { toNewUserEntry } from '../utils/usersUtils';
+import bcrypt from 'bcrypt';
 
-const createUser = async (newUser: NewUserEntry) => {
+const passwordHash = async (plainPassword: string): Promise<string> => {
+  const hash = await bcrypt.hash(plainPassword, 10);
+
+  return hash;
+};
+
+const getAllUsers = async () => {
+  const users = await User.findAll({
+    include: [
+      {
+        model: Post
+      },
+      {
+        model: Topic
+      }
+    ]
+  });
+
+  return users;
+};
+
+const getUserById = async (id: number) => {
+  const user = await User.findByPk(id);
+
+  return user;
+};
+
+const createUser = async (userInfo: any) => {
   try {
+    let newUser: NewUserEntry = toNewUserEntry(userInfo);
+
+    const hash = await passwordHash(newUser.passwordHash);
+    newUser = {
+      ...newUser,
+      passwordHash: hash 
+    };
+
     const createdUser = await User.create(newUser);
     return createdUser;
   } catch (err) {
     if (err instanceof UniqueConstraintError) {
       throw new Error(`${err.errors[0].message}`);
     }
-    throw new Error('something unexpected happened');
+    throw new Error('something unexpected happened' + err);
   }
 
 };
 
 export default {
+  getAllUsers,
+  getUserById,
   createUser
 };
