0250a96c9815103ed59a72c4d58a659d1ae6209a
PE5I <116784810+PE5I@users.noreply.github.com>
2023-07-01 12:45:07 -0700

    add cascade deletion for topics and posts if user is deleted

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,10 @@ import Post from './post';
 import Topic from './topic';
 import User from './user';
 
-User.hasMany(Post);
+User.hasMany(Post, { onDelete: 'cascade', hooks: true });
 Post.belongsTo(User);
 
-User.hasMany(Topic);
+User.hasMany(Topic, { onDelete: 'cascade', hooks: true });
 Topic.belongsTo(User);
 
 Topic.hasMany(Post);
