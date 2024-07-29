f9294b2477c6375b4753edede78bd1bcd7ef6abe
Pesi Taototo <ptaototo3@gatech.edu>
2024-07-29 12:50:42 -0700

    type login

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import loginService from '../services/loginService';
 // import loginService from '../services/loginService';
 const router = express.Router();
 
 // login user
-router.post('/', async (req, res) => {
+router.post('/', async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   const token = await loginService.verifyLogin(username, password);
